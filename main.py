5c13ad12c4358a69c853daf0e2295a96f7cb9c48
KarolinaZawisza <zawiszakarolina.t@gmail.com>
2021-09-02 14:19:54 +0200

    Automatising post/update

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import datetime as dt
 import os
 from dotenv import load_dotenv
 
+today_words: int
+
 load_dotenv('C:/Users/zawis/Documents/EV/.env')
 
 today = dt.datetime.now().strftime('%Y%m%d')
@@ -16,6 +18,8 @@ graph_endpoint = f'{PIXELA_ENDPOINT}/{USERNAME}/graphs'
 headers = {
     'X-USER-TOKEN': TOKEN
 }
+
+dates = ['20210901']
 
 def create_user():
     user_params = {
@@ -39,32 +43,50 @@ def create_graph():
 
     requests.post(url=graph_endpoint, json=graph_params, headers=headers)
 
-def graph_post(date: str, quantity: str):
+def graph_post(date: str, quantity: int):
+    global today_words
+    today_words = quantity
+
     post_endpoint = f'{PIXELA_ENDPOINT}/{USERNAME}/graphs/{ID}'
     post_params = {
         'date': date,
-        'quantity': quantity,
+        'quantity': str(today_words),
     }
 
     response = requests.post(url=post_endpoint, json=post_params, headers=headers)
     print(response.text)
+    print(f'First post: added {today_words} words at {date}')
 
-def graph_update(date, quantity):
+def graph_update(date: str, quantity: int):
+    global today_words
+    today_words += quantity
+
     update_endpoint = f'{PIXELA_ENDPOINT}/{USERNAME}/graphs/{ID}/{date}'
     update_params = {
-        'quantity': quantity
+        'quantity': str(today_words)
     }
 
     response = requests.put(url=update_endpoint, json=update_params, headers=headers)
     print(response.text)
+    print(f'Update: added {today_words} words at {date}')
 
-def graph_delete(date):
+def graph_delete(date: str):
     delete_endpoint = f'{PIXELA_ENDPOINT}/{USERNAME}/graphs/{ID}/{date}'
 
     response = requests.delete(url=delete_endpoint, headers=headers)
     print(response.text)
 
+    global today_words
+    today_words = 0
 
-user_words = input('How much words have you read today? ')
+def graph_config(date: str, quantity: int):
+    if date != dates[len(dates) - 1]:
+        dates.append(date)
+        print(dates)
+        graph_post(date, quantity)
+    elif date == dates[len(dates) - 1]:
+        graph_update(date, quantity)
 
-graph_update(today, user_words)
+
+user_words = int(input('How much words have you read today? '))
+graph_config(today, user_words)
